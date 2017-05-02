60758a634f298131d04d75f0dfea71a26a67f1a9
Nick Frichette <frichetten@gmail.com>
2017-05-02 17:45:38 -0500

    Made some improvements and am testing with POST requests. Django seems much more secure by default than Spring MVC. I've barely started and I'm already working with CSRF and XSS protections. This is a blast!

--- Testing/views.py
+++ Testing/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from Testing.models import File
 
@@ -8,10 +9,15 @@ def home(request):
         context = {'data': 'You are logged in ' + request.user.username}
     else:
         context = {'data': 'You are not logged in'}
+    file = File.objects.all()
+    print(file.values_list())
     return render(request, "home.html", context)
 
 
 def test(request):
     if request.method != "POST":
         return HttpResponse("<h1>Fail</h1>")
+    file = File(name=request.POST.get("name"), description=request.POST.get("description"))
+    file.save()
+    return HttpResponseRedirect("/")
 
